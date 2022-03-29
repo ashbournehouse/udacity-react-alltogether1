cd67b438e96fa5b793b060fa32bf98d96f62b4b8
Andrew <a.biller@ashho.com>
2022-03-29 18:11:02 +0000

    Add local state and input fields as wireframe to AddUser as wireframe

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './Images/logo.svg';
 import './Css/App.css';
 
 import Header from './Components/Header.jsx'
--- src/Components/AddUser.jsx
+++ src/Components/AddUser.jsx
@@ -1,14 +1,51 @@
 import React from 'react';
 import '../Css/AddUser.css';
 
-// Use a stateless functional component instead of a class ... for now.
 
-function AddUser() {
-  return (
-    <div className="Add-user">
-      <h4 className="Add-user-description">This div is rendered by the AddUser Component</h4>  
-    </div>
-  )
+class AddUser extends React.Component {
+  
+  state = {
+    userNameEntry: '',
+    firstNameEntry: '',
+    lastNameEntry: '',
+  };
+  
+  handleUserName = (event) => {this.setState({ userNameEntry: event.target.value })};
+  handleFirstName = (event) => {this.setState({ firstNameEntry: event.target.value })};
+  handleLastName = (event) => {this.setState({ lastNameEntry: event.target.value })};
+  
+  render() {
+    return (
+      <div className="add-user">
+        <h4 className="add-user-description">This div is rendered by the AddUser Component</h4>
+        <h5 className="input-label">Username: <span className="required-indicator">*</span></h5>
+        <input
+          className="text-input"
+          type="text"
+          placeholder="Enter Username"
+          value={this.state.userNameEntry}
+          onChange={this.handleUserName}
+        />
+        <h5 className="input-label">First Name: <span className="required-indicator">*</span></h5>
+        <input
+          className="text-input"
+          type="text"
+          placeholder="Enter First Name"
+          value={this.state.firstNameEntry}
+          onChange={this.handleFirstName}
+        />
+        <h5 className="input-label">Last Name: <span className="required-indicator">*</span></h5>
+        <input
+          className="text-input"
+          type="text"
+          placeholder="Enter Last Name"
+          value={this.state.lastNameEntry}
+          onChange={this.handleLastName}
+        />
+        <p className="required-field-note"><span className="required-indicator">*</span> indicates a required field</p>
+      </div>
+    )
+  }
 }
 
 export default AddUser
